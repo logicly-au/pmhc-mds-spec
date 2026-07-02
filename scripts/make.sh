b83ec0dd295ff703823a73aab308836b3f45cc48
Adam Clarke <adamc@logicly.com.au>
2026-07-02 14:18:26 +1000

    linting fixes

--- scripts/make.sh
+++ scripts/make.sh
@@ -3,9 +3,11 @@
 # Exit if anything errors
 set -e
 
+# shellcheck source=/dev/null
 source doc/version.env
 export SPEC_VERSION
 
+# shellcheck source=/dev/null
 source config.sh
 
 docker pull docker.sdlocal.net/csvw/metadata2rst:multiplatform
@@ -36,6 +38,7 @@ cd doc
 rm -rf _data build
 #cp -rf ../data _data
 
+# shellcheck disable=SC2034 # they are used in the docker invocation
 GIT_VERSION=$(git describe --tags --always)
 
 echo "Building PDF"
@@ -48,12 +51,12 @@ docker compose run \
 popd
 
 echo "Optimising images"
-docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
+docker run --rm -e GIT_VERSION -v .:/mnt/workdir \
   --workdir /mnt/workdir/doc/build/singlehtml/_images \
   logicly/sphinx-html2pdf:production \
   find . -name *.png -exec pngquant --force --output {} 8 {} \;
 
-docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
+docker run --rm -e GIT_VERSION -v .:/mnt/workdir \
   logicly/sphinx-html2pdf:production \
   /mnt/workdir/scripts/make-pdf.pl \
   --spec-name "${SPEC_NAME}-${SPEC_VERSION}" \
